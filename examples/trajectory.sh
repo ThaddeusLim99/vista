9672172966bffb3cca1875926ce9eeb52cd762c8
Sangwon Lim <sangwon3@ualberta.ca>
2022-11-14 10:29:32 -0700

    free up the space

--- examples/trajectory.sh
+++ examples/trajectory.sh
@@ -8,6 +8,7 @@ for FILE in /media/sangwon/My\ Book/Data\ Collection\ 2017/LiDAR_LAS/*.las.zip
 do
   for i in {10000..1000000..10000}
   do
+    rm /tmp/lidar/*.las
     python ./examples/conversion/las_to_h5.py --input /media/sangwon/My\ Book/Data\ Collection\ 2017/LiDAR_LAS/"${FILE##*/}" --frame $i || break
     python ./examples/basic_usage/sim_lidar.py --trace-path ./examples/vista_traces/lidar
   done
