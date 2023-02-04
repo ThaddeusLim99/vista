ea30d9f8cfb67d12143cbca60253826b5a745fa0
Sangwon Lim <sangwon3@ualberta.ca>
2023-02-04 12:38:21 -0700

    use torch tensor

--- examples/conversion/convert_single.py
+++ examples/conversion/convert_single.py
@@ -5,10 +5,14 @@ import numpy as np
 import zipfile
 import math
 import os
+import torch
 import pandas as pd
 
 
 def main(args):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu:0"
+    print(device)
+
     if args.input.endswith(".zip"):
         with zipfile.ZipFile(args.input, "r") as zip_ref:
             zip_ref.extractall("/tmp/lidar")
@@ -18,6 +22,7 @@ def main(args):
         print(f"/tmp/lidar/{args.input.split('/')[-1].split('.zip')[0]}")
 
     las = laspy.read(args.input)
+    # print(las.X, las.Y, las.Z)
 
     road_points = pd.read_csv(
         "./examples/Trajectory/road_points.csv", sep=",", header=None
@@ -30,19 +35,18 @@ def main(args):
     ).values
 
     trajectory = road_points * 1000
-
     i = args.frame
     print(f"Frame #: {i}")
 
-    pov_X = trajectory[i][0]
-    pov_Y = trajectory[i][1] - 5000000000
+    pov_X = trajectory[i][0] % 1000000
+    pov_Y = trajectory[i][1] % 1000000
     pov_Z = trajectory[i][2]
 
     x = np.array(las.X)
     y = np.array(las.Y)
     z = np.array(las.Z)
 
-    xyz = np.asarray([x, y, z], dtype=np.float64).T
+    xyz = np.asarray([x, y, z], dtype=np.float64).T / 100
 
     # Traslantion
     xyz -= np.array([pov_X, pov_Y, pov_Z])
@@ -51,13 +55,18 @@ def main(args):
         np.square(xyz[:, 0]) + np.square(xyz[:, 1]) + np.square(xyz[:, 2])
     )
 
-    indices = np.where((xyz_distance < 245000))
-    xyz = xyz[indices]
+    indices = np.where((xyz_distance < 100000))
+    xyz = torch.tensor(xyz[indices]).to(device)
 
     # Rotation 1
     cos_1 = forwards[i][0] / ((forwards[i][0] ** 2 + forwards[i][1] ** 2) ** (0.5))
     sin_1 = forwards[i][1] / ((forwards[i][0] ** 2 + forwards[i][1] ** 2) ** (0.5))
-    xyz = np.dot(np.array([[cos_1, sin_1, 0], [-sin_1, cos_1, 0], [0, 0, 1]]), xyz.T).T
+    xyz = torch.matmul(
+        torch.tensor([[cos_1, sin_1, 0], [-sin_1, cos_1, 0], [0, 0, 1]])
+        .double()
+        .to(device),
+        xyz.double().T,
+    ).T
 
     # Rotation 2
     cos_2 = ((forwards[i][0] ** 2 + forwards[i][1] ** 2) ** (0.5)) / (
@@ -66,17 +75,28 @@ def main(args):
     sin_2 = forwards[i][2] / (
         ((forwards[i][0] ** 2 + forwards[i][1] ** 2) + forwards[i][2] ** 2) ** (0.5)
     )
-    xyz = np.dot(np.array([[cos_2, 0, -sin_2], [0, 1, 0], [sin_2, 0, cos_2]]), xyz.T).T
+    xyz = torch.matmul(
+        torch.tensor([[cos_2, 0, -sin_2], [0, 1, 0], [sin_2, 0, cos_2]])
+        .double()
+        .to(device),
+        xyz.double().T,
+    ).T
 
     tangent = leftwards[i][2] / ((leftwards[i][0] ** 2 + leftwards[i][1] ** 2) ** (0.5))
     cross_angle = math.atan(tangent)
     cos_3 = math.cos(cross_angle)
     sin_3 = math.sin(cross_angle)
 
-    xyz = np.dot(np.array([[1, 0, 0], [0, cos_3, -sin_3], [0, sin_3, cos_3]]), xyz.T).T
+    xyz = torch.matmul(
+        torch.tensor([[1, 0, 0], [0, cos_3, -sin_3], [0, sin_3, cos_3]])
+        .double()
+        .to(device),
+        xyz.double().T,
+    ).T
 
     # Sensor at 1.2 meter above
     xyz[:, 2] -= 1200
+    xyz = xyz.cpu().numpy()
 
     with h5py.File(
         f"./examples/vista_traces/lidar_{args.process}/lidar_3d.h5", "w"
