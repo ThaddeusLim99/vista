14373b93349d82e0acbb75bcefd894b927fd2355
Sangwon Lim <sangwon3@ualberta.ca>
2023-02-04 12:38:50 -0700

    update rocesses

--- examples/single/process_6.sh
+++ examples/single/process_6.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in {5..4004..8} 
+for i in {5..571..8} 
 do
-    python ./examples/conversion/convert.py --input ./examples/vista_traces/03210N_C1R1_16000_20000.las --frame ${i} --process 6
+    python ./examples/conversion/convert_single.py --input ./examples/vista_traces/74202W_C1L1_L1L1_08000_06000.las --frame ${i} --process 6
     python ./examples/basic_usage/sim_lidar.py --trace-path ./examples/vista_traces/lidar_6 --resolution 0.05 --frame ${i}
     rm ./examples/vista_traces/lidar_6/lidar_3d*
 done
--- examples/single/process_8.sh
+++ examples/single/process_8.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in {7..4004..8} 
+for i in {7..571..8} 
 do
-    python ./examples/conversion/convert_single.py --input ./examples/vista_traces/03210N_C1R1_16000_20000.las --frame ${i} --process 8
+    python ./examples/conversion/convert_single.py --input ./examples/vista_traces/74202W_C1L1_L1L1_08000_06000.las --frame ${i} --process 8
     python ./examples/basic_usage/sim_lidar.py --trace-path ./examples/vista_traces/lidar_8 --resolution 0.05 --frame ${i}
     rm ./examples/vista_traces/lidar_8/lidar_3d*
 done
