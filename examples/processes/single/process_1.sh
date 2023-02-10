1d67452c4753520c2a036a39adc10b1a92500e61
Sangwon Lim <sangwon3@ualberta.ca>
2023-02-10 10:53:50 -0700

    processes bash scripts

--- examples/processes/single/process_1.sh
+++ examples/processes/single/process_1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in {0..571..4} 
+for i in 219 215 153
 do
     python ./examples/conversion/convert_single.py --input ./examples/vista_traces/74202W_C1L1_L1L1_08000_06000.las --frame ${i} --process 1
-    python ./examples/basic_usage/sim_lidar.py --trace-path ./examples/vista_traces/lidar_1 --resolution 0.05 --frame ${i}
+    python ./examples/basic_usage/sim_lidar.py --trace-path ./examples/vista_traces/lidar_1 --resolution 0.1 --frame ${i}
     rm ./examples/vista_traces/lidar_1/lidar_3d*
 done
--- examples/processes/unzip/unzip_1.sh
+++ examples/processes/unzip/unzip_1.sh
@@ -3,6 +3,6 @@ echo "Bash version ${BASH_VERSION}..."
 
 file_list=(`ls /media/sangwon/My\ Book1/Data\ Collection\ 2017/LiDAR_LAS/*.las.zip | sed -r 's/^.+\///' | sort`)
 length=${#file_list[@]}
-for (( i = 5; i < length; i+=8 )); do
+for (( i = 0; i < length; i+=4 )); do
   python ./examples/conversion/unzip.py --input /media/sangwon/My\ Book1/Data\ Collection\ 2017/LiDAR_LAS/"${file_list[$i]}"
 done
