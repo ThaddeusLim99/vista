bb447284475974354159d2c8d2098078e9f514d7
Sangwon Lim <sangwon3@ualberta.ca>
2023-02-10 14:51:27 -0700

    halt unzipping when storage space is >40%

--- examples/conversion/unzip.py
+++ examples/conversion/unzip.py
@@ -25,7 +25,7 @@ def main(args):
 
     print(f"Un-zipping to /tmp/lidar/{args.input.split('/')[-1].split('.zip')[0]}")
     total, used, free = shutil.disk_usage("/")
-    while used / total < 0.4:
+    while used / total > 0.4:
         total, used, free = shutil.disk_usage("/")
         time.sleep(60)
     with zipfile.ZipFile(args.input, "r") as zip_ref:
