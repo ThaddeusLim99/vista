02449591cabe8ef03dcb09ab8a0af7122d10c01f
Mohamed Abohassan <abohassa@ualberta.ca>
2023-04-06 13:55:04 -0600

    Started work on automating all the processes

--- examples/processes/single/process_1.sh
+++ examples/processes/single/process_1.sh
@@ -3,7 +3,7 @@
 file=$LASFILE
 total=`cat examples/Trajectory/forwards.csv | wc -l`
 
-for i in {1..$total..6}
+for i in `seq 1 $total 6`
 do
     python ./examples/conversion/convert_single.py --input ./examples/vista_traces/${file} --frame ${i} --range 100 --process 1
     python ./examples/basic_usage/sim_lidar.py --trace-path ./examples/vista_traces/lidar_1 --filename ${file} --frame ${i} --resolution 0.1 --yaw-min 180 --yaw-max 180 --pitch-min -60 --pitch-max 30
--- examples/processes/single/process_controller.sh
+++ examples/processes/single/process_controller.sh
@@ -6,9 +6,10 @@ LASFILE="01A02E_C1R1_16000_20000_START_HZ.las"
 start=1
 end=$processes
 
+
 export LASFILE
 for (( i=$start; i<=$processes; i++ ))
 do
-    xterm -hold -e bash examples/processes/single/process/process_${i}.sh &
+    xterm -T "Process ${i}" -hold -e bash examples/processes/single/process_${i}.sh &
     echo "Process ${i} of ${end} started"
 done
